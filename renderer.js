528352b35a8d9729a0a3d6c666e2456141643134
Nic Fontaine <ngpfontaine@gmail.com>
2016-11-29 18:02:01 -0800

    spawn chrome on ubuntu w/ --incognito param

--- renderer.js
+++ renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+// Launch local app (Linux app & path)
+var child = require('child_process').execFile
+var executablePath = "/usr/bin/google-chrome"
+var parameters = ["--incognito"];
+child(executablePath, parameters, function(err, data) {
+     console.log(err)
+     console.log(data.toString())
+})
+
